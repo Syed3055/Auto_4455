af2e3e8e74d9c31bf43f2b36d54aa8cc6a1c7937
Syed3055 <sajidashaik142@gmail.com>
2025-09-30 22:28:31 +0530

    Committing 4 items from workspace 30f8a50a-382d-4896-bd91-9dabd6668d23

---
+++ Fabric_item/Ware_4455.Warehouse/dbo/Views/location_view.sql
@@ -0,0 +1,2 @@
+-- Auto Generated (Do not modify) 7E259F41D85B55535B46068948362F2BF06EA79600E467B2B248544108BDF10B
+create    view location_view as select * from [Lake_4455].[dbo].[location_wise_sales]
---
+++ Fabric_item/Ware_4455.Warehouse/dbo/Views/sales_view.sql
@@ -0,0 +1,2 @@
+-- Auto Generated (Do not modify) 988E4920ADC76A31CFFFF56362510D7B4B53C0A4993C66EEB7C2814F6BB4CE13
+create   view sales_view as select * from [Lake_4455].[dbo].[Sales_data]
