6a28e12150c2f7c2235abcb4ec162e56b127f4c5
Syed3055 <sajidashaik142@gmail.com>
2025-10-01 00:11:36 +0530

    New note book 2

---
+++ Fabric_item/Notebook 2.Notebook/notebook-content.py
@@ -0,0 +1,22 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# CELL ********************
+
+from pyspark.sql.functions import *
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
